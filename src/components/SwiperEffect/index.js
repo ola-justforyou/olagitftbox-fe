fed972019e0a3cee30907a4c5576e32dddbddd55
faizmaruf <faiz.maruf8888@gmail.com>
2023-10-24 21:17:58 +0700

    create category component

---
+++ src/components/SwiperEffect/index.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import 'swiper/css/pagination';
+
+import '../SwiperEffect/style.css';
+
+import { EffectCreative, Pagination } from 'swiper/modules';
+
+const SwiperEffect = (props) => {
+  return (
+    <>
+      <Swiper
+        pagination={true}
+        grabCursor={true}
+        effect={'creative'}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: ['-20%', 0, -1],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+          },
+        }}
+        modules={[EffectCreative, Pagination]}
+        className='mySwiper3'
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default SwiperEffect;
--- src/views/App.js
+++ src/views/App.js
@@ -10,6 +10,7 @@ import {
   getAllProductsCategories,
   getAllProductsByCategory,
 } from '../actions/productAction';
+import SwiperEffect from '../components/SwiperEffect';
 function App(props) {
   const {
     state,
@@ -49,12 +50,41 @@ function App(props) {
     getDataProductsCategories();
     getDataProductsByCategory('smartphones');
   }, []);
+  const newArray = Array.from({ length: 12 }, () => ({
+    title: 'Judul Film Acak',
+    releaseYear: Math.floor(Math.random() * 1000) + 1000,
+  }));
+
   console.log(categories, 'categories');
   console.log(products, 'products');
   return (
     <div className='w-screen h-screen flex'>
-      <div className='m-auto'>
+      <div className='mx-auto container gap-y-6 flex flex-col'>
         <h1>hello world</h1>
+        <SwiperEffect />
+        <div className='text-white text-3xl  ml-7 sm:ml-0'>
+          <h2 className='text-lg font-medium text-black mb-2'>Category</h2>
+          <div className='flex flex-col h-full h w-full overflow-auto no-scrollbar overflow-y-hidden '>
+            <div className='flex min-w-min h-full text-black sm:gap-x-20  gap-x-6 justify-between pr-8'>
+              {newArray.map((item, i) => (
+                <div className='flex flex-col rounded-lg h-24 w-16 bg-blue-900'>
+                  <div
+                    className='basis-11/12 relative'
+                    onClick={(e) => console.log(e)}
+                  >
+                    <img
+                      src={''}
+                      className='object-cover rounded-3xl shadow-2xl h-full z-0 overflow-hidden'
+                    />
+                  </div>
+                  <p className='mt-1 mx-auto basis-1/12 text-white font-bold text-sm flex flex-col'>
+                    test
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
